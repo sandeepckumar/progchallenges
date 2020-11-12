97c473e689d5981d17413eac7d7d7b470ae35324
Sandeep Chenna <sandeepkchenna@gmail.com>
2020-11-12 14:30:16 +0530

    solved

--- 06-GenPrimes.js
+++ 06-GenPrimes.js
@@ -1,19 +1,32 @@
 /* Generate N prime numbers in given range */
 
-const genPrimes = (start, end) => {
-  let primes = [];
-  while (start < end) {
-    let i = 2;
-    while (i < start) {
-      if (start % i === 0) {
-        i++;
-      } else if (i === start) {
-        primes.push(i);
-      }
+const isPrime = (n) => {
+  if (n === 2) {
+    return true;
+  }
+  if (n % 2 === 0 || n === 0 || n === 1) {
+    return false;
+  }
+  div = Math.floor(Math.sqrt(n));
+  for (let i = 3; i <= div; i += 2) {
+    if (n % i === 0) {
+      return false;
     }
-    start++;
+  }
+  return true;
+};
+
+const genPrimes = (x, y) => {
+  let primes = [];
+  if (x % 2 === 0) {
+    x += 1;
+  }
+  for (let i = x; i <= y; i += 2) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
   }
   return primes;
 };
 
-console.log(genPrimes(1, 10));
+console.log(genPrimes(0, 100));
