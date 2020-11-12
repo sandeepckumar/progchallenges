a718c421029135fb7acfe3260bf00573f667b866
Sandeep Chenna <sandeepkchenna@gmail.com>
2020-11-12 12:41:46 +0530

    updated comments

--- 04-SubtractWoMinus.js
+++ 04-SubtractWoMinus.js
@@ -1,6 +1,6 @@
 /* Subtract two number without using minus operator*/
 
-// If x & y are given numbers, below implementation will add x to the 1's complement plus 1 of y which is x - y
+// If x & y are given numbers, below implementation will add x to the 2's complement of y which is x - y
 
 const minus1s = (x, y) => {
   return x + (~y + 1);
